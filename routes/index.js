71e2456908882a44766de06409c399deb0e6e637
eks05 <eungyo307@naver.com>
2021-08-18 22:33:50 +0900

    first

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router
-.get('/', function(req, res) { res.render("../views/blookly") })
+.get('/', function(req, res) { 
+    // res.render("../views/blookly") 
+    console.log('good')
+})
 
 
 module.exports = router;
