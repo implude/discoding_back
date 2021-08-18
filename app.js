fef39adefc9f9f554aba120df4fa5c31f8bcd5f6
eks05 <eungyo307@naver.com>
2021-08-18 22:12:03 +0900

    first

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const login = require('./routes/login');
+const PORT = process.env.PORT
 
 const app = express();
 
@@ -19,6 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'))
 // app.use('/account', require('./routes/account'));
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
   console.log('Listening at port number 3000') //포트는 원하시는 번호로..
 })
