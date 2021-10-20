fdf1a06921fca7d41b40d1d35368b457d076a98c
eks05 <eungyo307@naver.com>
2021-10-20 20:27:57 +0900

    aa

--- routes/bot.js
+++ routes/bot.js
@@ -8,77 +8,72 @@ const fs = require("fs");
 
 const vm = new NodeVM({
   require: {
-    timeout: 5000,
     external: true
   }
 });
-const vm2 = new NodeVM({
-  require: {
-    external: true
-  }
-});
-
 
 router
-  //봇 테스트 파트
-  .get("/test", (req, res) => {
-    let code = req.body.bot_code;
-    vm.run(`${code}`, "vm.js");
-    res.send("OK")
-  })
   //봇 정보 불러오기
   .get('/bot_info', (req, res) => {
     let name = req.body.bot_name
-    connect.query("SELECT * FROM created_bot", (err, rows, fields) => {
+    connect.query("SELECT * FROM created_bot WHERE bot_name = ?", [name], (err, rows, fields) => {
       for (let i = 0; i < rows.length; i++) {
-        if (rows[i].bot_name == name) {
-          res.send(JSON.stringify({
-            bot_name: rows[0].bot_name,
-            des: rows[0].des,
-            bot_id: rows[0].bot_id,
-            token: rows[0].token,
-            bot_img: rows[0].bot_img
-          }))
-        }
+        res.send(JSON.stringify({
+          bot_name: rows[0].bot_name,
+          des: rows[0].des,
+          bot_id: rows[0].bot_id,
+          token: rows[0].token,
+          bot_img: rows[0].bot_img
+        }))
       }
     })
   })
   //봇 만들기
-  .get('/create_bot', (req, res) => {
+  .post('/create_bot', (req, res) => {
     let name = req.body.bot_name
-    let code = req.body.bot_code
-
-    let info = [req.body.uuid, req.body.bot_name, req.body.des, req.body.token, req.body.bot_code]
-    connect.query("SELECT * FROM created_bot", (err, rows, fields))
-    for (let i = 0; i < rows.length; i++) {
-      if (rows[i].bot_name == name) {
-        res.send({ msg: '봇 이름이 중복됩니다.' })
-      } else {
-        connect.query('INSERT INTO created_bot(uuid, bot_name, des, token, js_code) VALUES (?, ?, ?, ?, ?)', info, (err, rows, fields) => {
-          if (err) { res.send(JSON.stringify({ msg: 'error' })) };
-          fs.writeFile(`./public/bot-codes/${name}`, code, (err) => {
-            if (err === null) {
-              console.log('success');
-            } else {
-              console.log('fail');
+    console.log(req.body);
+    let info = [crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.userid).digest("base64"), req.body.bot_name, req.body.des, req.body.token, req.body.bot_img]
+    connect.query("SELECT * FROM created_bot", (err, rows, fields) => {
+      for (let i = 0; i < rows.length; i++) {
+        if (rows[i].bot_name == name) {
+          res.send(JSON.stringify({ msg: 'error' }))
+          break;
+        } else {
+          connect.query('INSERT INTO created_bot(uuid, bot_name, des, token, bot_img) VALUES (?, ?, ?, ?,?)', info, (err, rows, fields) => {
+            if (err) { res.send(JSON.stringify({ msg: 'error' })) }
+            else {
+              res.send(JSON.stringify({ msg: 'OK' }))
             }
           })
-        });
+        }
       }
-    }
-  })
-
-  //봇 호스팅하기
-  .get('/hosting_bot', (req, res) => {
-    let name = req.body.bot_name
-    fs.readFile(`${name}`, 'utf-8', (err, data) => {
-      vm2.run(`${data}`, "vm.js");
     })
   })
-  .get('/hosting_bot_stop', (req, res) => {
+  .post('/edit_bot', (req, res) => {
+
+  })
+  .post('/hosting_page', (req, res) => {
+    let userid = req.body.userid
+    connect.query("SELECT * FROM user WHERE uuid = ?", [userid], (err, rows, fields) => {
+      console.log(userid)
+      res.send(JSON.stringify({
+        remaintime: rows[0].remain_time
+      }))
+    })
+  })
+  //봇 호스팅하기 
+  .get('/hosting_bot', (req, res) => {
     let name = req.body.bot_name
+    let code = req.body.js_code
+    fs.writeFile(`./public/bot-codes/${name}`, code, (err) => {
+      if (err === null) {
+        console.log('success');
+      } else {
+        console.log('fail');
+      }
+    })
     fs.readFile(`${name}`, 'utf-8', (err, data) => {
-      vm2.run(`${data}` + 'setTimeout(() => { exit() }, 1000)', "vm.js")
+      vm.run(`${data}`, "vm.js");
     })
   })
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const connect = require("../config/database").connection;
+const config = require("../routes/config");
+const crypto = require("crypto");
 
 router
   //처음 connection되었을 때 
   .get('/', (req, res) => {
     console.log('connection');
-
+    res.send("OK")
   })
-  // .post('/member', (req, res) => {
-  //   res.send(JSON.stringify({
-  //     a: "OK11",
-  //     b: "ghhfldsfaih",
-  //     c: "ghhfldsfaih"
-  //   }))
-  //   console.log(req.body)
-  // })
 
-  .post('/online', (req, res) => {
-    console.log(req.body)
+  .get('/online', (req, res) => {
     let a = "=discoding";
     for (let i = 0; i < 50; i++) {
       let math = Math.floor((Math.random() + 1) * 65);
@@ -26,64 +19,58 @@ router
         a = String.fromCharCode(math) + a;
       }
     }
-    res.send(JSON.stringify({ val1: a }))
+
+    res.send(JSON.stringify({ uuid: a }));
   })
+
 
   //유저 토큰이 존재 X
   .post("/new", (req, res) => {
-    let hashed_uuid = crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.uuid).digest("base64");
-    connect.query("INSERT INTO user(uid, remain_time) VALUES (?, ?)", [hashed_uuid, 0], (err, rows, fields) => {
+    console.log(req.body.UUID)
+    let hashed_uuid = crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.UUID).digest("base64");
+    connect.query("INSERT INTO user(uuid, remain_time) VALUES (?, ?)", [hashed_uuid, 0], (err, rows, fields) => {
       if (err) { res.send(JSON.stringify({ msg: "error" })) }
     }
     )
   })
   //유저 토큰이 존재 O
   .post("/login", (req, res) => {
-    connect.query("SELECT * FROM user", (err, rows, fields) => {
+    let bot_name = []
+    let des = []
+    let img = []
+    let hashed_uuid = crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.UUID).digest("base64");
+    console.log(hashed_uuid)
+    connect.query("SELECT * FROM created_bot where uuid=?", [hashed_uuid], (err, rows, fields) => {
       if (err) { res.send(JSON.stringify({ msg: "error" })) }
       else {
-        let hashed_uuid = crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.token).digest("base64");
         for (let i = 0; i < rows.length; i++) {
-          if (rows[i].uuid == hashed_uuid) {
-            get_bot_apply(hashed_uid)
-          }
+          bot_name.push(rows[i].bot_name)
+          des.push(rows[i].des)
+          img.push(rows[i].bot_img)
         }
+        res.send(JSON.stringify({
+          bot_name: bot_name,
+          des: des,
+          img: img
+        }))
       }
     })
   })
   //봇 시간 증가파트
   .get("/get-time", (req, res) => {
-    connect.query("INSERT INTO user(remaiin_time) VALUES (?)", [req.body.uuid, req.body.remain_time], (err, rows, fields) => {
-      if (err) { res.send(JSON.stringify({ msg: "error" })) }
-    }
-    );
+    let remain_time
+    let hashed_uuid = crypto.createHmac(config.crypto_key1, config.crypto_key2).update(req.body.uuid).digest("base64");
+    connect.query(`SELECT * FROM user WHERE uuid=?`, [hashed_uuid], (err, rows, fields) => {
+      remain_time = rows[0].remain_time
+      connect.query("UPDATE user SET remain_time = ? WHERE uuid = ?", [req.body.get_time + remain_time, hashed_uuid], (err, rows, fields) => {
+        if (err) { res.send(JSON.stringify({ msg: "error" })) }
+      }
+      );
+    })
   })
   .get("/block-coding", function (req, res) {
     res.render("../views/blookly");
   })
 
-
-function get_bot_apply(uuid) {
-  connect.query("SELECT * FROM created_bot", (err, rows, fields) => {
-    let bot_name = []
-    let des = []
-    let img = []
-    if (err) { res.send({ msg: 'error' }) }
-    else {
-      for (let i = 0; i < rows.length; i++) {
-        if (rows[i].uuid == uuid) {
-          bot_name.push(rows[i].bot_name)
-          des.push(rows[i].des)
-          img.push(rows[i].bot_img)
-        }
-      }
-      res.send(JSON.stringify({
-        bot_name: bot_name,
-        des: des,
-        img: img
-      }))
-    }
-  })
-}
 module.exports = router;
 
