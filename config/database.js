4136fcaeb94830244e5027fbf3041b953430bdf3
eks05 <eks0220@naver.com>
2021-12-17 11:27:26 +0900

    fd

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     port: '3306',
     user: 'root',
-    password: 'sansogknnamu0203',
+    password: '98o0lerswdxi8ukl',
     database: 'test'
 })
 
