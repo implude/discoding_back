d99813d79fc05efd77f02aa4ae644f0cf43f8d97
eks05 <eks0220@naver.com>
2021-12-20 10:24:59 +0900

    dasf

--- routes/bot.js
+++ routes/bot.js
@@ -107,7 +107,7 @@ router
   .post('/get_bot_token', (req, res) => {
     let sql = "SELECT * FROM created_bot WHERE bot_name = ?"
     connect.query(sql, [req.body.name], (err, rows, fields) => {
-      console.log(rows)
+      if (err) res.send(err)
       res.send(JSON.stringify({
         token: rows[0].token
       }))
